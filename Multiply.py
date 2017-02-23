fb83254b314758eeaa15d1270499510df6d7672c
yxw163 <yangxw163@gmail.com>
2017-02-23 23:15:41 +0800

    add multipy

---
+++ Multiply.py
@@ -0,0 +1,13 @@
+#! /usr/local/bin python3 
+# -*- coding: utf-8 -*-
+"""
+	Description: 
+		The code does not execute properly. Try to figure out why.
+
+    Author: 
+    	yangxw163@gmail.com
+    Created:
+    	22 Ferbary 2017
+"""
+def multiply(a, b):
+    return a * b
