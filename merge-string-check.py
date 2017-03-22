131d8390027fbfdc85f73e5f70560dc407aeecbd
yxw163 <yangxw163@gmail.com>
2017-03-22 10:08:47 +0800

    modified merge-string-check

--- merge-string-check.py
+++ merge-string-check.py
@@ -1,15 +1,15 @@
-#! /usr/local/bin python3 
+#! /usr/local/bin python3
 # -*- coding: utf-8 -*-
 """
-	Description: 
-		At a job interview, you are challenged to write an algorithm 
-		to check if a given string, s, can be formed from two other strings, 
+	Description:
+		At a job interview, you are challenged to write an algorithm
+		to check if a given string, s, can be formed from two other strings,
 		part1 and part2.
 
-		The restriction is that the characters in part1 and part2 
+		The restriction is that the characters in part1 and part2
 		are in the same order as in s.
 
-		The interviewer gives you the following example and tells 
+		The interviewer gives you the following example and tells
 		you to figure out the rest from the given test cases.
 
 	Example:
@@ -23,32 +23,29 @@
     Args:
         s (string), part1 (string), part2 (string)
 
-    Author: 
+    Author:
 
     	yangxw163@gmail.com
-        
+
     Created:
 
     	27 Ferbary 2017
 """
 
-def is_merge(s, part1, part2):
-	result = s[:]
-	if len(s) != len(part1) + len(part2):
-		return False
-	step = 0
-	for i in range(0, len(part1)):
-		if result[step:].find(part1[i]) >= 0:
-			step = result.index(part1[i])
-			result = result.replace(part1[i],'')
-		else:
-			return False
 
-	if result == part2:
+def is_merge(s, part1, part2):
+	if not part1:
+		return s == part2
+	if not part2:
+		return s == part1
+	if not s:
+		return part1 + part2 == ''
+
+	if s[0] == part1[0] and is_merge(s[1:], part1[1:], part2):
 		return True
-	else:
-		return False
-	
+
+	if s[0] == part2[0] and is_merge(s[1:], part1, part2[1:]):
+		return True
 
 if __name__ == '__main__':
 	print(is_merge('Bananas', 'Bahamas', ''))
